46fbc6d05a9b705d4a52896adb648e50376f972a
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-03-19 18:52:58 -0300

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ig.news | Início</title>
+        <title>Ig.news | Home</title>
       </Head>
-      <h2>Home</h2>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey Welcome!</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>For $ 9.90 month</span>
+          </p>
+        </section>
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          width={330}
+          height={498}
+        />
+      </main>
     </>
   );
 }
