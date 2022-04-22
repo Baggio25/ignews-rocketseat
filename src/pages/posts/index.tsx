4967ef646ad0e930bd6b4ca80b059cb0abda8349
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-04-22 15:32:17 -0300

    Listing post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,25 @@
+/* eslint-disable react/jsx-key */
 import Head from "next/head";
 import { GetStaticProps } from "next";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from './../../services/prismic';
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -14,33 +27,13 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2022</time>
-            <strong>Código Limpo: reflexão e prática</strong>
-            <p>
-              Desenvolvimento de software possui diversas bases teóricas que definem
-              o comportamento da programação e ajudam a padronizar métodos
-              de criação
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2022</time>
-            <strong>Código Limpo: reflexão e prática</strong>
-            <p>
-              Desenvolvimento de software possui diversas bases teóricas que definem
-              o comportamento da programação e ajudam a padronizar métodos
-              de criação
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2022</time>
-            <strong>Código Limpo: reflexão e prática</strong>
-            <p>
-              Desenvolvimento de software possui diversas bases teóricas que definem
-              o comportamento da programação e ajudam a padronizar métodos
-              de criação
-            </p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
 
         </div>
       </main>
@@ -52,18 +45,29 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const response = await prismic.query([
+  const response = await prismic.query<any>([
     Prismic.predicates.at("document.type", "publication")
   ], {
     fetch: ["publication.title", "publication.content"],
     pageSize: 100
   })
 
-  console.log(response);
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric"
+      })
+    }
+  })
 
   return {
     props: {
-
+      posts
     }
   }
 }
