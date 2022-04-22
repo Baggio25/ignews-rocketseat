cc3ea7be01683c71837968c6a83bdf3cd30924f6
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-04-22 14:56:38 -0300

    Consume Prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,9 @@
-
 import Head from "next/head";
+import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
+
+import { getPrismicClient } from './../../services/prismic';
+
 import styles from "./styles.module.scss";
 
 export default function Posts() {
@@ -42,4 +46,24 @@ export default function Posts() {
       </main>
     </>
   )
-} 
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at("document.type", "publication")
+  ], {
+    fetch: ["publication.title", "publication.content"],
+    pageSize: 100
+  })
+
+  console.log(response);
+
+  return {
+    props: {
+
+    }
+  }
+}
